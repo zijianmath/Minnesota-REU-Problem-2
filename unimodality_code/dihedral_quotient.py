a9a916773e4be20f5b27ac8557dae2d5f6160b31
Aaron Landesman <aaronlandesman@gmail.com>
2014-07-01 22:05:45 -0500

    adding dihedral group

--- unimodality_code/dihedral_quotient.py
+++ unimodality_code/dihedral_quotient.py
@@ -4,23 +4,6 @@ import math
 from operator import mul    # or mul=lambda x,y:x*y
 from fractions import Fraction
 
-def prime_fact(n):
-    if n <= 2:
-        return [n]
-    lst = []
-    while n > 2:
-        for i in range(2,n):
-            if n % i == 0:
-                n = n/i
-                lst.append(i)
-                if n <= 2:
-                    lst.append(2)
-                    return lst
-                break
-            if i == n-1:
-                lst.append(n)
-                return lst
-
 def pairs(n,i,r):
     lst = range(n)
     size_i_sets = combinations(lst,i)
@@ -45,45 +28,57 @@ def edge_cycles(n,(y,x)):
                 all_cycles.append((tuple(sorted(j)),tuple(sorted(i))))
     return all_cycles
 
-def orbit_cycles(n,(y,x)):
+def dihedral_involution(n,x):
+    return map(lambda t: (n-t)%n,x)
+
+def cyclic_single_orbit(n,x):
+    return map(lambda i: map(lambda t: (t+i)%n,x),range(n))
+
+def dihedral_single_orbit(n,x):
+    reverse_x = dihedral_involution(n,x)
+    return cyclic_single_orbit(n,x) + cyclic_single_orbit(n,reverse_x)
+
+def dihedral_edges(n,(x,y)):
     all_cycles = []
-    for i in xrange(n):
-        all_cycles.append((tuple(sorted(map(lambda t: (t+i)%n,y))),tuple(sorted(map(lambda t: (t+i)%n,x)))))
+    x_cycles = dihedral_single_orbit(n,x)
+    y_cycles = dihedral_single_orbit(n,y)
+    for i in x_cycles:
+        for j in y_cycles:
+            if set(i).issubset(set(j)):
+                all_cycles.append((tuple(sorted(j)),tuple(sorted(i))))
     return all_cycles
+
+def cyclic_double_orbit(n,(x,y)):
+    cyclic_orbits = []
+    for i in xrange(n):
+        cyclic_orbits.append((tuple(sorted(map(lambda t: (t+i)%n,y))),tuple(sorted(map(lambda t: (t+i)%n,x)))))
+    return cyclic_orbits
+
+def dihedral_double_orbit(n,(x,y)):
+    reverse_xy = (dihedral_involution(n,x),dihedral_involution(n,y))
+    return cyclic_double_orbit(n,(x,y)) + cyclic_double_orbit(n,reverse_xy)
 
 #print all_cycles(4,((1,2),(1,)))
 
-def cyclic_p_i(n,i,r):
+def dihedral_p_i(n,i,r):
     pairs_set = pairs(n,i,r)
     reduced_pairs = []
     while len(pairs_set) > 0:
         elt = pairs_set.pop()
         reduced_pairs.append(elt)
-        cycles = edge_cycles(n,elt)
+        cycles = dihedral_edges(n,elt)
         for i in cycles:
             if i in pairs_set:
                 pairs_set.remove(i)
     return reduced_pairs
 
-def cyclic_p_i(n,i,r):
+def dihedral_q_i(n,i,r):
     pairs_set = pairs(n,i,r)
     reduced_pairs = []
     while len(pairs_set) > 0:
         elt = pairs_set.pop()
         reduced_pairs.append(elt)
-        cycles = edge_cycles(n,elt)
-        for i in cycles:
-            if i in pairs_set:
-                pairs_set.remove(i)
-    return reduced_pairs
-
-def cyclic_q_i(n,i,r):
-    pairs_set = pairs(n,i,r)
-    reduced_pairs = []
-    while len(pairs_set) > 0:
-        elt = pairs_set.pop()
-        reduced_pairs.append(elt)
-        cycles = orbit_cycles(n,elt)
+        cycles = dihedral_double_orbit(n,elt)
         for i in cycles:
             if i in pairs_set:
                 pairs_set.remove(i)
@@ -93,11 +88,6 @@ def cyclic_q_i(n,i,r):
 
 #print cyclic_p_i(3,2,1)
 
-def nCk(n,k): 
-    return int( reduce(mul, (Fraction(n-i, i+1) for i in range(k)), 1) )
-
-def calculate_p_i(n,i):
-    return nCk(n-1,i-1)
 '''
 for n in range (3,8):
     for i in range(2,n+1):
@@ -113,11 +103,16 @@ for n in range (3,15):
     print 'finished ' + str(n)
 '''
 
-for n in range (13,17):
-    for i in range(7,n+1):
-        print (n,i,len(cyclic_p_i(n,i,7)))
+for n in range (2,13):
+    for i in range(1,n+1):
+        print (n,i,len(dihedral_q_i(n,i,1)))
     print 'finished ' + str(n)
 
+'''for n in range (2,7):
+    for i in range(1,n+1):
+        print (n,i,len(dihedral_q_i(n,i,1)))
+    print 'finished ' + str(n)
+'''
 
 
 
